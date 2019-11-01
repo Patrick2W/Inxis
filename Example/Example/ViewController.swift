9d66824bc987e14543d4286a184fb3656f1d05b6
Patrick.W <cheyun@cheyundeiMac.local>
2019-11-01 17:02:18 +0800

    instance

---
+++ Example/Example/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Example
+//
+//  Created by cheyun on 2019/11/1.
+//  Copyright © 2019 ixxxxx.com. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Inxis/Inxis/Inxis.h
@@ -0,0 +1,19 @@
+//
+//  Inxis.h
+//  Inxis
+//
+//  Created by cheyun on 2019/11/1.
+//  Copyright © 2019 ixxxxx.com. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Inxis.
+FOUNDATION_EXPORT double InxisVersionNumber;
+
+//! Project version string for Inxis.
+FOUNDATION_EXPORT const unsigned char InxisVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Inxis/PublicHeader.h>
+
+
